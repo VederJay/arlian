25ccfa1a924c6e43c4d88ae5a679d5f067e8d4af
jeroen <jeroen.vandeweghe@kbc.be>
2020-12-22 11:28:14 +0100

    got application testing to work with h2

--- src/test/java/org/arlian/site/SiteApplicationTests.java
+++ src/test/java/org/arlian/site/SiteApplicationTests.java
@@ -2,8 +2,10 @@ package org.arlian.site;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SiteApplicationTests {
 
 	@Test
