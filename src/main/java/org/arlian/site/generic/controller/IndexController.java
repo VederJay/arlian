bbfcbffc1f2e5f7b47ddaed323c445e3ab2319ee
jeroen <jeroen.vandeweghe@kbc.be>
2020-12-30 09:54:36 +0100

    forwarding if authenticated + rename generic controller

--- src/main/java/org/arlian/site/generic/controller/IndexController.java
+++ src/main/java/org/arlian/site/generic/controller/IndexController.java
@@ -1,14 +1,20 @@
 package org.arlian.site.generic.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class NotLoggedInController {
+public class IndexController {
 
     @GetMapping({"", "/", "index", "/index"})
-    public String index(){
-        return "pages/index";
+    public String index(Authentication authentication){
+
+        if(authentication==null)
+            return "pages/index";
+
+        else
+            return "redirect:/start";
     }
 
     @GetMapping("privacy")
