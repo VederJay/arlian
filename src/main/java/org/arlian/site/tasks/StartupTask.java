3e3c2c778edb5acc27e11d817d5e56ccbbf06638
jeroen <jeroen.vandeweghe@kbc.be>
2021-04-13 16:25:49 +0200

    removed startup task for setting up image link thumbnails (they're automatically added for new ones in future)

--- src/main/java/org/arlian/site/tasks/StartupTask.java
+++ src/main/java/org/arlian/site/tasks/StartupTask.java
@@ -1,39 +1,15 @@
 package org.arlian.site.tasks;
 
-import org.apache.commons.imaging.ImageReadException;
-import org.apache.commons.imaging.ImageWriteException;
-import org.arlian.site.start.model.link.Link;
-import org.arlian.site.start.model.link.LinkRepository;
-import org.arlian.site.start.service.ImageService;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-import java.util.List;
-
 @Component
 public class StartupTask {
 
-    private final LinkRepository linkRepository;
-    private final ImageService imageService;
-
-    public StartupTask(LinkRepository linkRepository, ImageService imageService) {
-        this.linkRepository = linkRepository;
-        this.imageService = imageService;
-    }
 
     @EventListener(ApplicationReadyEvent.class)
-    public void fixMissingThumbnails() throws ImageWriteException, ImageReadException, IOException {
-        List<Link> links = linkRepository.findAll();
+    public void doSomething() {
 
-        for (Link link : links) {
-            if(link.getImage() != null && link.getThumbnail() == null){
-                byte[] imageBytes = link.getImage();
-                imageService.addImageToLink(link, imageBytes);
-            }
-        }
-
-        linkRepository.saveAll(links);
     }
 }
